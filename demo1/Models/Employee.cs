da6537f32015ddc96bedc4127e30f62f06eb4b2c
fa6om <fa6om@LAPTOP-4OBBIE72>
2024-12-24 14:11:37 +0300

    Add project files.

---
+++ demo1/Models/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1.Models
+{
+    public class Employee
+
+    {
+       public string Name;
+       public string Email;
+        public string Gender;
+
+
+        public string GetInfo(string gender, string name, string email)
+        {
+            string result;
+            if(gender =="M")
+            {
+                result = $"welcom Mr ,{name}, Email is : {email}";
+
+            }
+            else
+            {
+                result = $"welcom Ms ,{name}, Email is : {email}";
+            }
+        return result ;
+        }
+
+        public string GetInfo(string fname, string mname, string lname,int age)
+        {
+            return "first name" +fname+"" + "mid name"+mname+"" +"last name "+ lname+"age ; " +""+ age;
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Get Info,no retern taype");
+
+        }
+
+
+
+
+    }
+}
---
+++ demo1/Program.cs
@@ -0,0 +1,63 @@
+﻿//using Namespace
+// object class
+//fill and use code by objectName 
+// Display data 
+//ClassName opName=new className();
+
+//task 1 
+
+using demo1.Models;
+using System.Net.Mime;
+using System.Runtime.Versioning;
+Employee employee = new Employee();
+//employee.Name = "fatma";
+//employee.Email = "fatma.com";
+//employee.Gender = Console.ReadLine().Trim().ToUpper();
+
+//Console.WriteLine("M|F");
+//employee.Gender = (Console.ReadLine());
+
+//if (employee.Gender == "f")
+//{
+//    Console.WriteLine(" welcom Ms " + employee.Name + "  email is :" + employee.Email);
+
+//}
+//else
+//{
+//    Console.WriteLine(" welcom Mr " + employee.Name + "  email is :" + employee.Email);
+//}
+
+/////task 2 
+
+
+////object class
+////Employee employee = new Employee();
+
+////para
+//string name, gender, email;
+//Console.WriteLine("Enter Name");
+//name=Console.ReadLine();
+//Console.WriteLine("Enter Email");
+//email = Console.ReadLine();
+//Console.WriteLine("Enter Gender (M|F");
+//gender = Console.ReadLine();
+
+///assing value to function 
+ //Console.WriteLine(employee.GetInfo(gender,name,email));
+
+
+string  fname, mname, lname;
+int age;
+Console.WriteLine("4 per(First name,mname,lnameand age");
+fname= Console.ReadLine();
+mname= Console.ReadLine(); 
+lname = Console.ReadLine();
+age=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(employee.GetInfo(fname,mname,lname,age));
+
+
+
+
+
+
+
